e62da1cb4c6e270eabe4365bccaed1ed889ba69c
Vu Nguyen <phantomtet16698@gmail.com>
2025-08-06 13:53:10 +0700

    .

---
+++ app/(tabs)/video/index.tsx
@@ -0,0 +1,9 @@
+import { ThemedView } from "@/components/ThemedView";
+import { Button } from "@react-navigation/elements";
+
+export default function IndexPage() {
+
+    return <ThemedView>
+        <Button>aaaaaa</Button>
+    </ThemedView>
+}
